09d5d150c9564cd16ab0085d4fa9c6dff726a73f
Windell Oskay <windell@oskay.net>
2015-06-07 10:44:16 -0700

    Fine tuning.
    Update readme, wrap main processing function in a call (so that it
    could potentially added with a listener as well). Add hooks for
    third-party candidates back in.

--- README.md
+++ README.md
@@ -5,34 +5,12 @@ Chrome extension that replaces the names of major political candidates with the 
 
 Specifically, those running for the office of President of the United States of America in the 2016 cycle. 
 
-Options allow you to poopify the names of Democrats, Republicans, or both. (Any major third-party candidates that emerge will also be added to the list.)
+Options allow you to "poopify" the names of Democrats, Republicans, or both. (Any major third-party candidates that emerge will also be added to the list.) 
 
-
-
-Really?
-=============
-
-Yes.
-
-
-What is your political agenda, here? 
-=============
-
-No particular agenda, sorry. This is a public service project from Evil Mad Scientist Laboratories. 
-
-Possible reasons that you might want to use this extension might include:
-
-* You are from outside the US, and don't need to hear these names every day.
-* You are temporarily overloaded by the amount of poop that the candidates sling at one another.
-* For humor's sake.
-
-
-No judgement upon any of the named individuals, nor their platforms, parties, or beliefs is either implied or intended. We are offering an equal-opportunity text replacement tool, for the good of all humanity.
 
 
 Installation
 =============
-
 
 [Direct download of crx file](https://github.com/oskay/politicians-to-poop/blob/master/pol-to-poo.crx?raw=true)
 
@@ -46,7 +24,24 @@ What about FireFox, Safari, Opera?
 Sorry, not yet. Pull requests and/or pointers to versions for those browsers will be happily accepted.
 
 
-List of participants
+
+What is the political agenda here? 
+=============
+
+No particular agenda, sorry. This is a public service project from Evil Mad Scientist Laboratories. 
+
+Possible reasons that you might want to use this extension might include:
+
+* You are from outside the US, and don't need to hear these names every day.
+* You are temporarily overloaded by the amount of poop that the candidates sling at one another.
+* For humor's sake.
+
+
+No judgement upon any of the named individuals, nor their platforms, parties, or beliefs is either implied or intended. This is an equal-opportunity text replacement tool, for the good of all humanity.
+
+
+
+List of named participants
 =============
 
 The following individuals are currently identified as "major" political candidates, meeting the necessary criteria for inclusion in this list:
@@ -57,6 +52,14 @@ Democrats: Hillary Clinton, Bernie Sanders, Lincoln Chafee, and Martin O'Malley.
 
 Third-party and independent candidates: None, at present.
 
+
+
+Known issues
+=============
+
+Text replacement does not reliably work in some circumstances, including on pages of Google search results. 
+
+If you identify other circumstances where text replacement does not work reliably (or generates too many false positives), you are welcome to open an issue. 
 
 
 
--- Source/content_script.js
+++ Source/content_script.js
@@ -1,3 +1,4 @@
+function process_page() {
 chrome.storage.sync.get({
 	democrats: true,
 	republicans: true,
@@ -5,58 +6,87 @@ chrome.storage.sync.get({
     extended: false
   }, function(items) {
 	  
-	  var addShortNames = false; 
+	var addShortNames = false; 
 	var addDemocrats = true; 
 	var addRepublicans = true; 
 	var addOthers = true; 
 
-	var SearchString = 'poop';
-// 	    console.log(addDemocrats);
+	var SearchString = '';
 	addDemocrats = items.democrats;
     addRepublicans = items.republicans;
     addOthers = items.thirdparty;    
     addShortNames = items.extended;   
 
-if (addRepublicans == true)
-	{
-		SearchString += '|Rafael Edward "Ted" Cruz|Rafael Edward Cruz|Ted Cruz';
-		SearchString += '|Carly Fiorina|Cara Carleton Sneed|Ms. Fiorina|Mrs. Fiorina|Fiorina';
-		SearchString += '|Ben Carson|Benjamin Solomon';
-		SearchString += '|Marco Rubio|Marco Antonio Rubio|Rubio';
-		SearchString += '|Richard John "Rick" Santorum|Richard John Santorum|Rick Santorum|Santorum';
-		SearchString += '|Lindsey Olin Graham|Lindsey Graham|Senator Graham';
-		SearchString += '|George Elmer Pataki|George Pataki|Pataki';
-		SearchString += '|Randal Howard "Rand" Paul|Randal Howard Paul|Rand Paul|Senator Paul';
-		SearchString += '|Mike Huckabee|Michael Dale "Mike" Huckabee|Michael Dale Huckabee|Michael Huckabee|Governor Huckabee|Huckabee';
-		SearchString += '|Rick Perry|Governor Perry|James Richard "Rick" Perry|James Richard Perry';
-		
-		SearchString += '|John Ellis "Jeb" Bush|John Ellis Bush|Jeb Bush'; 
-		
-		if ( addShortNames == true)
+	if (addRepublicans == true)
 		{
-			SearchString += "|Cruz|Carly|Carson|Graham|Pataki|Rand|Paul|Perry|Jeb";
+			// Note 1: Search terms must be listed in order from most specific to least specific.
+			
+			// Note 2: For the more uncommon (e.g., Santorum, Huckabee) we do not include titles.
+			// (Reasoning: "Senator 💩" is funnier than "💩" in most circumstances.)
+			// For more common names (e.g., Clinton, Cruz), we need to include more titles.
+			// The phrase "Senator Clinton" will (usually) identify Hillary but not Bill Clinton.
+			
+			SearchString += 'Rafael Edward "Ted" Cruz|Rafael Edward Cruz|Ted Cruz|Senator Cruz';
+			SearchString += '|Carly Fiorina|Cara Carleton Sneed|Ms. Fiorina|Mrs. Fiorina|Fiorina';
+			SearchString += '|Ben Carson|Benjamin Solomon';
+			SearchString += '|Marco Rubio|Marco Antonio Rubio|Rubio';
+			SearchString += '|Richard John "Rick" Santorum|Richard John Santorum|Rick Santorum|Santorum';
+			SearchString += '|Lindsey Olin Graham|Lindsey Graham|Senator Graham';
+			SearchString += '|George Elmer Pataki|George Pataki|Pataki';
+			SearchString += '|Randal Howard "Rand" Paul|Randal Howard Paul|Rand Paul|Senator Paul';
+			SearchString += '|Mike Huckabee|Michael Dale "Mike" Huckabee|Michael Dale Huckabee|Michael Huckabee|Huckabee';
+			SearchString += '|Rick Perry|Governor Perry|James Richard "Rick" Perry|James Richard Perry';
+			
+			SearchString += '|John Ellis "Jeb" Bush|John Ellis Bush|Jeb Bush'; 
+			
+			if ( addShortNames == true)
+			{
+				SearchString += "|Cruz|Carly|Carson|Graham|Pataki|Rand|Paul|Perry|Jeb|Governor Bush";
+			}
 		}
-	}
-
-if (addDemocrats == true)
-	{
-		SearchString += '|Hillary Diane Rodham Clinton|Hillary Rodham Clinton|Hillary Rodham|Hillary Clinton';
-		SearchString += '|Mrs. Clinton|Hillary Diane Rodham|Mrs. Bill Clinton|First Lady Clinton|Senator Clinton|Secretary of State Clinton|Secretary Clinton';
-		SearchString += '|Bernie Sanders|Senator Sanders';
-		SearchString += '|Lincoln Chafee|Governor Chafee|Senator Chafee|Chafee';
-		SearchString += "|Martin Joseph O'Malley|Martin O'Malley|Governor O'Malley|O'Malley";
-
-		if ( addShortNames == true)
-		{
-			SearchString += "|Hillary|Bernie|Clinton|Sanders|Rodham";
-		}
-	}
-
-	var re = new RegExp(SearchString, "gi");
-	$("body *").replaceText( re, "<span class='poopy'>💩</span>" );
-	//This jQuery based search and replace is respectful of tags-- replaces only text. Links still work!
 	
-	$("<style type='text/css'> .poopy{ font-style: normal !important;font-weight: normal !important ;font-family: serif !important;} </style>").appendTo("head");
+	if (addDemocrats == true)
+		{
+			if (SearchString != '')
+				{SearchString += '|';}
+			SearchString += 'Hillary Diane Rodham Clinton|Hillary Rodham Clinton|Hillary Rodham|Hillary Clinton';
+			SearchString += '|Mrs. Clinton|Hillary Diane Rodham|Mrs. Bill Clinton|First Lady Clinton|Senator Clinton|Secretary of State Clinton|Secretary Clinton';
+			SearchString += '|Bernie Sanders|Senator Sanders';
+			SearchString += '|Lincoln Chafee|Governor Chafee|Senator Chafee|Chafee';
+			SearchString += "|Martin Joseph O'Malley|Martin O'Malley|Governor O'Malley|O'Malley";
+	
+			if ( addShortNames == true)
+			{
+				SearchString += "|Hillary|Bernie|Clinton|Sanders";
+			}
+		}
 
+	
+	if (addOthers == true)
+		{
+			// No "major" notable candidates yet. That's what auto-updates are for, right? 
+/*			if (SearchString != '')
+				{SearchString += '|';}
+			SearchString += 'Roseanne Cherrie Barr|Roseanne Barr|Zoltan Istvan|Vermin Supreme'; 
+
+			if ( addShortNames == true)
+			{
+				SearchString += "";
+			}
+*/
+		}
+	
+		var re = new RegExp(SearchString, "gi");
+		$("body *").replaceText( re, "<span class='poopy'>💩</span>" );
+		//This jQuery based search and replace is respectful of tags-- replaces only text. Links still work!
+		
+		$("<style type='text/css'> .poopy{ font-style: normal !important;font-weight: normal !important ;font-family: serif !important;} </style>").appendTo("head");
+		//Emoji will often not display in a CSS container that is specified to be in bold face, italic, or a specific font.
+		//This CSS override will fix that in most (but not all) cases.
 });
 
+}
+
+
+
+process_page();
